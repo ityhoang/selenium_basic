c1a1f548d251da88dd31f9da7e71a81bda6d0f3c
Tý <tyhoang0462@gmail.com>
2024-10-30 22:41:23 +0700

    chore: done case login

--- src/selenium_basic/LoginTestCase.java
+++ src/selenium_basic/LoginTestCase.java
@@ -3,34 +3,74 @@ package selenium_basic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.WheelInput;
 
-public class LoginTestCase extends BaseChromeTest{
+public class LoginTestCase extends BaseChromeTest {
 	final String urlLogin = Constant.base_url + "login";
-	
+
 	@Test
-	public void shouldScrollFromViewportByGivenAmountFromOrigin() {
-		driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame.html");
+	public void LoginSuccess() throws InterruptedException {
+		driver.get(urlLogin);
 
-		WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin.fromViewport(10, 10);
-		new Actions(driver).scrollFromOrigin(scrollOrigin, 0, 200).perform();
+		WebElement userField = driver.findElement(By.cssSelector("input[formcontrolname=\"username\"]"));
+		WebElement passField = driver.findElement(By.cssSelector("input[formcontrolname=\"password\"]"));
+		userField.sendKeys("test1");
+		passField.sendKeys("12345678aA");
+		WebElement buttonLogin = driver.findElement(
+				By.cssSelector(".mdc-button.mdc-button--raised.mat-mdc-raised-button.mat-primary.mat-mdc-button-base"));
+		buttonLogin.click();
+		Thread.sleep(2000);
 
-		WebElement iframe = driver.findElement(By.tagName("iframe"));
-		driver.switchTo().frame(iframe);
-		WebElement checkbox = driver.findElement(By.name("scroll_checkbox"));
-		Assertions.assertTrue(inViewport(checkbox));
+		Assertions.assertTrue(!driver.getCurrentUrl().equals(urlLogin));
 	}
 
-	private boolean inViewport(WebElement element) {
+	@Test
+	public void LoginFail() throws InterruptedException {
+		driver.get(urlLogin);
 
-		String script = "for(var e=arguments[0],f=e.offsetTop,t=e.offsetLeft,o=e.offsetWidth,n=e.offsetHeight;\n"
-				+ "e.offsetParent;)f+=(e=e.offsetParent).offsetTop,t+=e.offsetLeft;\n"
-				+ "return f<window.pageYOffset+window.innerHeight&&t<window.pageXOffset+window.innerWidth&&f+n>\n"
-				+ "window.pageYOffset&&t+o>window.pageXOffset";
+		WebElement userField = driver.findElement(By.cssSelector("input[formcontrolname=\"username\"]"));
+		WebElement passField = driver.findElement(By.cssSelector("input[formcontrolname=\"password\"]"));
+		userField.sendKeys("test1");
+		passField.sendKeys("12345678aAa");
+		WebElement buttonLogin = driver.findElement(
+				By.cssSelector(".mdc-button.mdc-button--raised.mat-mdc-raised-button.mat-primary.mat-mdc-button-base"));
+		buttonLogin.click();
+		Thread.sleep(2000);
+//		WebElement errorMsg = driver.findElement(By.id("mat-mdc-error-1"));
+//		Boolean isError = errorMsg.getText().equals("Username or Password is incorrect.");
+		Boolean isChangeUrlFail = driver.getCurrentUrl().equals(urlLogin);
+		Assertions.assertTrue(isChangeUrlFail);
+	}
+	
+	@Test
+	public void LoginEmptyPass() throws InterruptedException {
+		driver.get(urlLogin);
 
-		return (boolean) ((JavascriptExecutor) driver).executeScript(script, element);
+		WebElement userField = driver.findElement(By.cssSelector("input[formcontrolname=\"username\"]"));
+		WebElement passField = driver.findElement(By.cssSelector("input[formcontrolname=\"password\"]"));
+		userField.sendKeys("test1");
+		passField.sendKeys("");
+		WebElement buttonLogin = driver.findElement(
+				By.cssSelector(".mdc-button.mdc-button--raised.mat-mdc-raised-button.mat-primary.mat-mdc-button-base"));
+		buttonLogin.click();
+		Thread.sleep(2000);
+
+		Assertions.assertTrue(driver.getCurrentUrl().equals(urlLogin));
+	}
+	
+	@Test
+	public void LoginEmptyUser() throws InterruptedException {
+		driver.get(urlLogin);
+
+		WebElement userField = driver.findElement(By.cssSelector("input[formcontrolname=\"username\"]"));
+		WebElement passField = driver.findElement(By.cssSelector("input[formcontrolname=\"password\"]"));
+		userField.sendKeys("");
+		passField.sendKeys("12345678aAa");
+		WebElement buttonLogin = driver.findElement(
+				By.cssSelector(".mdc-button.mdc-button--raised.mat-mdc-raised-button.mat-primary.mat-mdc-button-base"));
+		buttonLogin.click();
+		Thread.sleep(2000);
+
+		Assertions.assertTrue(driver.getCurrentUrl().equals(urlLogin));
 	}
 }
